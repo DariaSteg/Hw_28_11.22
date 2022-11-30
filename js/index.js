33f1ef579f972fe142d1743bec6a86fccf9e23e1
Daria Stegura <97463847+DariaSteg@users.noreply.github.com>
2022-11-30 09:12:59 +0200

    Refactor strc

---
+++ js/index.js
@@ -0,0 +1,16 @@
+"use strict";
+
+
+// const sentence = "     to         BE       oR   nOt        To     bE ";
+// function toJadenCase(str) {
+// return str
+// .replaceAll(' ','')
+// .toLowerCase('')
+// .split(" ")
+// .map((word) => word[0].toUpperCase() + word.substring(1))
+// .join(" ");
+// }
+// console.log(toJadenCase(sentence)); //повинен бути результат:  'To Be Or Not To Be'
+
+
+ 
